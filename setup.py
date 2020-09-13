c9f03935064ac5a0871bc70ffb13d183a2460e46
Reed Schimmel <reedschimmel@gmail.com>
2020-09-13 16:09:17 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="support_resistance_finder",
+    version="0.1.0",
+    url="https://github.com/Reed-Schimmel/support_resistance_finder.git",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'pandas'],
+    python_requires='>=3.6',
+)
